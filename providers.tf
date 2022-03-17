2d996f3abdc75a36b3ea1ce41f55c3e28ac861c5
Dyego Alekssander Maas <dyego.maas@gmail.com>
2022-03-16 22:44:44 -0300

    Configurando variável com secret do GCP.

--- providers.tf
+++ providers.tf
@@ -7,9 +7,14 @@ terraform {
   }
 }
 
+variable "GOOGLE_CREDENTIALS" {
+    type = string
+    description = "key for GCP"
+}
+
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
+  credentials = var.GOOGLE_CREDENTIALS
   project = "lab-devops-cloud-123"
   region  = "us-west1"
   zone    = "us-west1-b"
